70f124b3f502fb83987a96dfcabafa8b56f2a462
JuniorPaivadeAlcantara <142550329+JuniorPaivadeAlcantara@users.noreply.github.com>
2023-10-04 20:00:17 -0300

    components dinamycs propertys

--- src/app/components/big-card/big-card.component.ts
+++ src/app/components/big-card/big-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-big-card',
@@ -6,6 +6,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./big-card.component.css']
 })
 export class BigCardComponent implements OnInit {
+  @Input()
+  photoCover: string ="https://s2-techtudo.glbimg.com/S8ei00JGHti7zm18TQz3JwZUzEY=/0x0:995x560/1000x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_08fbf48bc0524877943fe86e43087e7a/internal_photos/bs/2020/J/6/OYDoBRSfOFGfZkQ8wbBg/captura-de-tela-2020-06-26-as-13.00.25.png"
+  @Input()
+  cardTitle: string="NOVO HOMEM DE FERRO ANUNCIADO"
+  @Input()
+  cardDescription: string ="Marvel Studios anuncia novo filme do homem de Ferro!"
 
   constructor() { }
 
